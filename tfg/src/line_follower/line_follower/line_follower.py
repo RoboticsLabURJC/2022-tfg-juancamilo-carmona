a7852cf9edbe534a2e57ce346b5421649c117f80
camilo_0715 <jc.carmona.2019@alumnos.urjc.es>
2023-03-22 18:08:28 +0100

    memory usage wrote in mb to csv file

--- tfg/src/line_follower/line_follower/line_follower.py
+++ tfg/src/line_follower/line_follower/line_follower.py
@@ -348,7 +348,7 @@ class VehicleTeleop(Node):
         memory_usage = self.process.memory_info().rss    
         cpu_percent = self.process.cpu_percent()
         
-        self.csv_writer.writerow([time.time() - self.program_start_time ,self.last_fps, cpu_percent , memory_usage, self.curling, abs(stering)])
+        self.csv_writer.writerow([time.time() - self.program_start_time ,self.last_fps, cpu_percent , memory_usage/(1024*1024), self.curling, abs(stering)])
 
             
 
